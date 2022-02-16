678c54f675278d9ced2d3528a99190b6ab4c8a91
blongngo28 <baolongngo928@gmail.com>
2022-02-16 17:50:33 +0700

    Edit WriteBack

--- wb.v
+++ wb.v
@@ -1,45 +1,21 @@
 module WB (
-
-    input   wire        rst,
-
-    input   wire[4:0]   MemWriteNum,
-    input   wire        MemWriteReg,
-    input   wire[31:0]  MemWriteData,
-
-    output  reg [4:0]   WriteBackNum,
-    output  reg         WriteBackReg,
-    output  reg [31:0]  WriteBackData
-
+    input rst,
+    input   wire[1:0]       WBSel,
+    input   wire[31:0] PC,
+    input   wire[31:0] ALUOut,
+    input   wire[31:0]  Data_from_mem,
+    output  reg[31:0]  DataWriteToReg
 );
 
-/*
- * This always part controls the signal WriteBackNum.
- */  
 always @ (*) begin
     if (rst)
-        WriteBackNum <= 5'b0;
-    else
-        WriteBackNum <= MemWriteNum;
-end
-
-/*
- * This always part controls the signal WriteBackReg.
- */
-always @ (*) begin
-    if (rst)
-        WriteBackReg <= 1'b0;
-    else
-        WriteBackReg <= MemWriteReg;
-end
-
-/*
- * This always part controls the signal WriteBackData.
- */
-always @ (*) begin
-    if (rst)
-        WriteBackData <= 32'b0;
-    else
-        WriteBackData <= MemWriteData;
+        DataWriteToReg <= 32'b0;
+    else if (WBSel == 2'b0)
+        DataWriteToReg <= Data_from_mem;
+    else if (WBSel == 2'b01)
+        DataWriteToReg <= ALUOut;
+    else if (WBSel == 2'b10)
+        DataWriteToReg <= PC + 4;
 end
 
 endmodule
